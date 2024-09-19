0f32464ab2e3a2f56020ca79f94748eb7c54b5dd
LuanEscritorio <luanlima747@gmail.com>
2024-09-18 23:28:03 -0300

    ajustes finais

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,29 @@
+import entities.Bill;
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner sc = new Scanner((System.in));
+        Bill bill = new Bill();
+
+        System.out.print("Sexo: ");
+        bill.gender = sc.next().toUpperCase().charAt(0);
+        while (bill.gender != 'M' && bill.gender != 'F') {
+            System.out.print("Sexo incorreto, Digite (F/M): ");
+            bill.gender = sc.next().toUpperCase().charAt(0);
+        }
+        System.out.print("Quantidade de cerveja: ");
+        bill.beer = sc. nextInt();
+
+        System.out.print("Quantidade de refrigerante: ");
+        bill.softDrink = sc.nextInt();
+
+        System.out.print("Quantidade de espetinho: ");
+        bill.barbecue = sc.nextInt();
+
+        System.out.println("\nRELATÓRIO: ");
+        System.out.println(bill);
 
     }
 }
--- src/entities/Bill.java
+++ src/entities/Bill.java
@@ -1,32 +1,43 @@
 package entities;
 
+import java.sql.SQLOutput;
+
 public class Bill {
     public char gender;
     public int beer;
-    public int barbercue;
+    public int barbecue;
     public int softDrink;
 
-    public double cover(){
-        if(beer + barbercue + softDrink < 30){
+    public double cover() {
+        if (beer * 5 + barbecue * 7 + softDrink * 3 < 30) {
             return 4.00;
-        }else{
+        } else {
             return 0;
         }
     }
 
-    public double feeding(){
-        return beer + barbercue + softDrink;
+    public double feeding() {
+
+        return beer * 5 + barbecue * 7 + softDrink * 3;
     }
 
-    public double ticket(){
-        if(gender == 'M'){
+    public double ticket() {
+        if (gender == 'M') {
             return 10.0;
-        }else{
+        } else {
             return 8.0;
         }
     }
 
-    public double total(){
+    public double total() {
         return ticket() + cover() + feeding();
+    }
+
+    @Override
+    public String toString() {
+        return "Consumo =  R$" + String.format("%.2f\n", feeding())
+                + "Couvert = R$" + String.format("%.2f\n", cover())
+                + "Ingresso = R$" + String.format("%.2f\n", ticket())
+                + "Valor Total = R$" + String.format("%.2f\n", total());
     }
 }
